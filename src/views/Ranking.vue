8c3b43222523ff72f6000c7546d2fd0521e58813
Gabriel Blank Stift Mousquer <gabriel.mousquer@digitaldesk.com.br>
2018-10-29 09:30:44 -0300

    tela ranking

--- src/views/Ranking.vue
+++ src/views/Ranking.vue
@@ -1,5 +1,35 @@
 <template>
   <div>
-    Tela <strong>Ranking</strong>
+    <b-table
+      class="tabela"
+      hover
+      :items="listaComIndice"
+    ></b-table>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      lista: [
+        {
+          nome: 'Gabriel',
+          pontuacao: 41
+        }
+      ]
+    }
+  },
+  computed: {
+    listaComIndice() {
+      return this.lista.map((x, i) => ({posicao: i + 1, ...x}))
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tabela {
+  text-align: center;
+}
+</style>
