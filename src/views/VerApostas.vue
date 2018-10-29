b6d62ef7b8d260b4ec8bd98c4c7ddcb4b86397be
Gabriel Blank Stift Mousquer <gabriel.mousquer@digitaldesk.com.br>
2018-10-29 09:22:44 -0300

    página ver apostas - dados estáticos

--- src/views/VerApostas.vue
+++ src/views/VerApostas.vue
@@ -2,6 +2,7 @@
   <div>
     <b-form class="filtros">
       <b-form-select
+        class="filtro"
         :options="rodadas"
         v-model="rodadaSelecionada"
       />
@@ -9,9 +10,15 @@
       <b-form-select
         :options="usuarios.map(x => ({text: x.nome, value: x.id}))"
         v-model="usuarioSelecionado"
-        class="input-sm"
+        class="input-sm filtro"
       />
     </b-form>
+
+    <b-list-group class="listagem">
+      <b-list-group-item v-for="(r, i) in resultados" :key="i" :variant="acertos.has(i - 1) ? 'success' : ''">
+        {{r.home}} {{r.actualScoreHome}} ({{r.betScoreHome}}) x ({{r.betScoreVisistor}}) {{r.actualScoreVisitor}} {{r.visitor}}
+      </b-list-group-item>
+    </b-list-group>
   </div>
 </template>
 
@@ -23,7 +30,41 @@ export default {
       rodadaSelecionada: 1,
       usuarios: [{ nome: 'Gabriel', id: 1 }, { nome: 'Ândres', id: 2 }, { nome: 'João', id: 3 }],
       usuarioSelecionado: 1,
-      resultados: null
+      resultados: [
+        {
+          home: 'Internacional',
+          visitor: 'Palmeiras',
+          actualScoreHome: 1,
+          betScoreHome: 2,
+          actualScoreVisitor: 1,
+          betScoreVisistor: 0
+        },
+        {
+          home: 'Gremio',
+          visitor: 'Sport',
+          actualScoreHome: 3,
+          betScoreHome: 3,
+          actualScoreVisitor: 4,
+          betScoreVisistor: 4
+        },
+        {
+          home: 'Flamengo',
+          visitor: 'Santos',
+          actualScoreHome: 3,
+          betScoreHome: 3,
+          actualScoreVisitor: 1,
+          betScoreVisistor: 0
+        }
+      ]
+    }
+  },
+  computed: {
+    acertos() {
+      const indices = this.resultados
+        .filter(r => r.betScoreHome === r.actualScoreHome && r.betScoreVisistor === r.actualScoreVisitor)
+        .map((r, i) => i)
+
+      return new Set(indices)
     }
   }
 }
@@ -31,7 +72,17 @@ export default {
 
 <style>
 .filtros {
-  width: 30%;
+  width: 250px;
+  margin: 0 auto;
+}
+
+.filtro {
+  margin-bottom: 5px;
+}
+
+.listagem {
+  text-align: center;
+  max-width: 400px;
   margin: 0 auto;
 }
 </style>
